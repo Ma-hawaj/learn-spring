dafd3183a6c7ab7d9bc54c52de25ef3a49aeaa76
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-19 15:24:44 +0300

    Adding scope=prototype example

--- src/main/java/com/mahmood/learn_spring/BusinessCalculation04/MySQLDataService.java
+++ src/main/java/com/mahmood/learn_spring/BusinessCalculation04/MySQLDataService.java
@@ -1,8 +1,13 @@
 package com.mahmood.learn_spring.BusinessCalculation04;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value= ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+// this annotation set the scope of the bean as prototype which
+// means that every time this bean is called it will return a new one
 public class MySQLDataService implements DataService{
     public int[] retrieveData(){
         return new int[] { 11, 22, 33, 44, 55 };
