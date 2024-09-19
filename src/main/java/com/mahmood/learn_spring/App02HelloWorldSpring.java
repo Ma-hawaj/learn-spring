da3a7eb26d2aa3d4d62e4014ad112c51c85cbc18
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-19 11:04:10 +0300

    finishing hello world spring

--- src/main/java/com/mahmood/learn_spring/App02HelloWorldSpring.java
+++ src/main/java/com/mahmood/learn_spring/App02HelloWorldSpring.java
@@ -2,6 +2,8 @@ package com.mahmood.learn_spring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
         //1: Launch a Spring Context
@@ -13,9 +15,21 @@ public class App02HelloWorldSpring {
         // retrieving by the bean name
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("person3Parameter"));
+
         // retrieving by the class name
         // note that there should be only one bean that returns this class
         System.out.println(context.getBean(Address.class));
+
+        // list all the beans names managed by spring
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        // prioritizing one address bean using @Primary
+        System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean("person3Parameter"));
+
+        // Using the qualifier to override primary
+        System.out.println(context.getBean("person4Parameter"));
+
     }
 
 }
--- src/main/java/com/mahmood/learn_spring/HelloWorldSpringConfiguration.java
+++ src/main/java/com/mahmood/learn_spring/HelloWorldSpringConfiguration.java
@@ -1,7 +1,9 @@
 package com.mahmood.learn_spring;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person(String Name, int age, Address address){};
 
@@ -27,24 +29,36 @@ public class HelloWorldSpringConfiguration {
     }
 
     @Bean
-    public Address address(){
+    @Primary // Added this to prioritize this bean between the candidates
+    public Address address1(){
         return new Address("5649", "Manama");
     }
 
     @Bean
+    @Qualifier("notDefault")
+    public Address address2(){ return new Address("north", "Muharaq");}
+
+    @Bean
     public Person person2MethodCall(){
-        return new Person(name(), age(), address());
+        return new Person(name(), age(), address1());
     }
 
     @Bean
-    public Person person3Parameter(String name, int age, Address address){
+    public Person person3Parameter(String name, int age, Address address2){
         /*
          * this is auto wiring with parameters
          * so spring wires the parameters with
          * beans that have the same name.
+         *
+         * The address will get the Address.class bean,
+         * and it doesn't care about the bean name!
          */
-        return new Person(name, age, address);
+        return new Person(name, age, address2);
     }
 
+    @Bean
+    public Person person4Parameter(String name, int age, @Qualifier("notDefault") Address address){
+        return new Person(name, age, address);
+    }
 
 }
