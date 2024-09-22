a29d6b068c23188ea0b83654272a4a5ac38a3a6e
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-22 12:50:44 +0300

    SpringBootApplication already do compScan

--- src/main/java/com/mahmood/learn_spring/springBootHello05/SpringBootHello05.java
+++ src/main/java/com/mahmood/learn_spring/springBootHello05/SpringBootHello05.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.mahmood.learn_spring.springBootHello05")
 public class SpringBootHello05 {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHello05.class, args);
