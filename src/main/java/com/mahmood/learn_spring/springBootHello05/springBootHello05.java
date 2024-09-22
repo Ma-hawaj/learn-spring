3c3987548b408c39096f3458cc7f79df13bb6ae3
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-22 10:16:20 +0300

    Starting with spring boot

---
+++ src/main/java/com/mahmood/learn_spring/springBootHello05/springBootHello05.java
@@ -0,0 +1,11 @@
+package com.mahmood.learn_spring.springBootHello05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springBootHello05 {
+    public static void main(String[] args) {
+        SpringApplication.run(springBootHello05.class, args);
+    }
+}
