958ab57efc1aa17e7a76f0196ca5bd69e1da8748
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-23 16:52:55 +0300

    Implementing error handeling

--- firstrestapi/src/main/java/com/mahmood/firstrestapi/socialmediaapi/UserController.java
+++ firstrestapi/src/main/java/com/mahmood/firstrestapi/socialmediaapi/UserController.java
@@ -23,7 +23,12 @@ public class UserController {
 
     @GetMapping(path = "/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return myService.findOne(id);
+        User user = myService.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException("id: " + id);
+        }
+
+        return user;
     }
 
     @PostMapping(path = "/users")
