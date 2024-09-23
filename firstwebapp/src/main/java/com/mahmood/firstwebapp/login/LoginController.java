d9168064b0d8f5733ea62b0fd995d616c73233e0
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-23 10:27:22 +0300

    Injecting variables to the jsp

--- firstwebapp/src/main/java/com/mahmood/firstwebapp/login/LoginController.java
+++ firstwebapp/src/main/java/com/mahmood/firstwebapp/login/LoginController.java
@@ -1,13 +1,19 @@
 package com.mahmood.firstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("login")
-    public String loginPage(){
+    public String loginPage(@RequestParam String name, ModelMap model){
+
+        // this is used to inject variables to the jsp
+        model.put("name", name);
+        System.out.println(name);
         return "login";
     }
 }
