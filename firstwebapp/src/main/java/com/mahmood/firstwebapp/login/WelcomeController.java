c3767889fbc4a94cf40511df4981e1a622130d2c
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-23 13:21:53 +0300

    Using security context

--- firstwebapp/src/main/java/com/mahmood/firstwebapp/login/WelcomeController.java
+++ firstwebapp/src/main/java/com/mahmood/firstwebapp/login/WelcomeController.java
@@ -1,22 +1,26 @@
 package com.mahmood.firstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("name") // this should be available in every class that uses the session
 public class WelcomeController {
 
+    private String getLoggedUsername(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getName();
+    }
+
     @RequestMapping(value="/")
     public String welcomePage(ModelMap model){
 
             // this is used to inject variables to the jsp
-            model.put("name", "Mahmood");
+            model.put("name", getLoggedUsername());
             return "welcome";
     }
 }
