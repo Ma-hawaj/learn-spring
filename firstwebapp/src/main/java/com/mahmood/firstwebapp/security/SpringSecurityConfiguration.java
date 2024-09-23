57491a614ce8b14e1ccad9b1513504b03c10c47c
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-23 14:15:54 +0300

    Configuring security to work with h2 console

--- firstwebapp/src/main/java/com/mahmood/firstwebapp/security/SpringSecurityConfiguration.java
+++ firstwebapp/src/main/java/com/mahmood/firstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.mahmood.firstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -34,5 +37,25 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // since we are configuring the filter chain we need to reconfigure the defaults also
+
+        // This enables auth on all requests
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+
+        // Configure the default from login
+        http.formLogin(Customizer.withDefaults());
+
+        // this to allow accessing the h2 console
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+
     }
 }
