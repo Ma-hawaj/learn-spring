fc5bab1f1f525dfd0bd29d605f09d85373609e2b
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-23 10:37:27 +0300

    Using the logger

--- firstwebapp/src/main/java/com/mahmood/firstwebapp/login/LoginController.java
+++ firstwebapp/src/main/java/com/mahmood/firstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.mahmood.firstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("login")
     public String loginPage(@RequestParam String name, ModelMap model){
 
         // this is used to inject variables to the jsp
         model.put("name", name);
-        System.out.println(name);
+        logger.debug("Request param is {}",name);
         return "login";
     }
 }
