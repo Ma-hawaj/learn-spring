d90d8dd899b445071804f920b26545a7f3c67c5b
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-24 12:12:46 +0300

    Trying static filtering for properties

---
+++ firstrestapi/src/main/java/com/mahmood/firstrestapi/filtering/FilteringController.java
@@ -0,0 +1,25 @@
+package com.mahmood.firstrestapi.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class FilteringController {
+
+    @GetMapping("/filtering")
+    public SomeBean filtering(){
+        return new SomeBean("v1", "v2", "v3");
+    }
+
+    @GetMapping("/filtering-list")
+    public List<SomeBean> filteringList(){
+        return Arrays.asList(
+                new SomeBean("v1", "v2", "v3"),
+                new SomeBean("v1", "v2", "v3"),
+                new SomeBean("v1", "v2", "v3")
+        );
+    }
+}
--- firstrestapi/src/main/java/com/mahmood/firstrestapi/socialmediaapi/User.java
+++ firstrestapi/src/main/java/com/mahmood/firstrestapi/socialmediaapi/User.java
@@ -1,5 +1,6 @@
 package com.mahmood.firstrestapi.socialmediaapi;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ public class User {
     private Integer id;
 
     @Size(min=2, message = "Name should have at least 2 chars")
+    @JsonProperty("usr_name") // this will override name in the output json
     private String name;
 
     @Past(message = "Date should be in the past")
