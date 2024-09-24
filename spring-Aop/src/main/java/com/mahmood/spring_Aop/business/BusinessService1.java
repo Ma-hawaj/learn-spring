3ce307ed8ed589e6dd0f7b54d45c33397b4cba4d
Mahmood Alhawaj <mahmood.alhawaj@array.world>
2024-09-24 16:28:33 +0300

    Initializing a new project

---
+++ spring-Aop/src/main/java/com/mahmood/spring_Aop/business/BusinessService1.java
@@ -0,0 +1,14 @@
+package com.mahmood.spring_Aop.business;
+
+public class BusinessService1 {
+    
+    private DataService1 dataService1;
+
+    public void calculateMax(){
+        int[] data = dataService1.retrieveData();
+        Ar
+    }
+
+    
+}
+
---
+++ spring-Aop/src/test/java/com/mahmood/spring_Aop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmood.spring_Aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
