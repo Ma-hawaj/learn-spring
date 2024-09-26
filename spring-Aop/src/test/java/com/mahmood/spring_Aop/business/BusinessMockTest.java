5cac0832105b1b90734d66430547457b72d18c08
Ma.hawaj <mahmood.alhawaj@array.world>
2024-09-26 09:24:54 +0300

    Using mockito annotations

--- spring-Aop/src/test/java/com/mahmood/spring_Aop/business/BusinessMockTest.java
+++ spring-Aop/src/test/java/com/mahmood/spring_Aop/business/BusinessMockTest.java
@@ -1,11 +1,23 @@
 package com.mahmood.spring_Aop.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 public class BusinessMockTest {
+
+    @Mock
+    private DataService1 dataService1Mock;
+
+    @InjectMocks
+    private BusinessService1 businessService1;
 
     @Test
     void findTheMax(){
@@ -15,5 +27,14 @@ public class BusinessMockTest {
 
         BusinessService1 businessService1 = new BusinessService1(dataServiceMock);
         assertEquals(5, businessService1.calculateMax() );
+    }
+
+    @Test
+    void findZeroUsingAnnotations(){
+
+        when(dataService1Mock.retrieveData()).thenReturn(new int[] {});
+
+        assertEquals(0, businessService1.calculateMax());
+
     }
 }
